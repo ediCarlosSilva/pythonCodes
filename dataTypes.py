72c8aa3970db6659c961b69b14b1908425cb024c
Edi Carlos <edi_csilva@hotmail.com>
2025-11-03 21:02:30 -0300

    studied data types

---
+++ dataTypes.py
@@ -0,0 +1,11 @@
+song1 = "213 seconds"
+song2 = 213 # this is a number (int)
+
+song3 = 213.5 # this is a (float)
+
+song4 = "150"
+song5 = 250.25
+
+playlist = int(song4) + (song5)
+print(f"The playlist is {playlist} seconds.")
+ 
