09dc7f7a27fcf792f42df582a9e366a365a86b9a
Edi Carlos <edi.carlos@divtech.com.br>
2025-11-29 06:18:57 -0300

    Project concluded

--- w05-lists/w05_project_shopping_cart.py
+++ w05-lists/w05_project_shopping_cart.py
@@ -1,3 +1,4 @@
+# added list of quantities to the cart in line 11. Logic for that in each option of the cart (lines 34 - 35, lines 40 - 46, line 64, and line 70)
 """
 Author: Edi Carlos
 Program: shopping Cart
@@ -7,6 +8,7 @@ Description: A program that stores a list of products in a shopping cart along w
 
 list_of_names = []
 list_of_prices = []
+list_of_quantities = []
 
 option = ""
 
@@ -29,11 +31,19 @@ while option != "5":
         item_price = float(input(f"What is the price of '{item_name}'? "))
         list_of_prices.append(item_price)
 
+        item_quantity = int(input(f"What is the quantity of '{item_name}'? "))
+        list_of_quantities.append(item_quantity)
+
         print(f"'{item_name}' has been added to the cart.")
     elif option == '2':
         print("The contents of the shopping cart are:")
+        total_amount = 0
         for i in range(len(list_of_names)):
-            print(f"{i + 1}. {list_of_names[i]} - ${list_of_prices[i]:.2f}")
+            amount = list_of_prices[i] * list_of_quantities[i]
+            total_amount += amount
+            print(f"{i + 1}. {list_of_names[i]} - ${list_of_prices[i]:.2f} - quantity: {list_of_quantities[i]} - amount: {amount:.2f}")
+        
+        print(f"\nThe total price of the items in the shopping cart is {total_amount}")
     elif option == '3':
         print("The contents of the shopping cart are:")
         for i in range(len(list_of_names)):
@@ -51,11 +61,12 @@ while option != "5":
             else:
                 list_of_names.pop(item_to_remove - 1)
                 list_of_prices.pop(item_to_remove - 1)
+                list_of_quantities.pop(item_to_remove - 1)
                 print("Item removed.")
     elif option == '4':
         amount = 0
-        for item in list_of_prices:
-            amount += item
+        for i in range(len(list_of_prices)):
+            amount += (list_of_prices[i] * list_of_quantities[i])
         print(f"The total price of the items in the shopping cart is ${amount}")
     elif option == '5':
         break
