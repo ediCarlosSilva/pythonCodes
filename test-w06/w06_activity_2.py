d1360c05cccb2a9ed14f780b5741fe9133dabb30
Edi Carlos <edi_csilva@hotmail.com>
2025-12-03 16:56:56 +0000

    activity 2 - week 6)

---
+++ test-w06/w06_activity_2.py
@@ -0,0 +1,24 @@
+"""
+Author: Edi Carlos
+purpose: train iterating through files
+"""
+
+people = [
+    "Stephanie 36",
+    "John 29",
+    "Emily 24",
+    "Gretchen 54",
+    "Noah 12",
+    "Penelope 32",
+    "Michael 2",
+    "Jacob 10"
+]
+
+youngest = int(people[0].split()[1])
+youngest_name = ""
+
+for person in people:
+    person_name = person[0]
+    person_age = person[1]
+
+
