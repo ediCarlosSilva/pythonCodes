e11411cb542a40efdecc696f662a111c4f66d929
Edi Carlos <edi.carlos@divtech.com.br>
2025-12-04 08:16:16 -0300

    tests

--- w06/test-w06/w06_activity_2.py
+++ w06/test-w06/w06_activity_2.py
@@ -18,7 +18,20 @@ youngest = int(people[0].split()[1])
 youngest_name = ""
 
 for person in people:
+    
+    print(person)
+    person = person.split()
+    print(person)
+
     person_name = person[0]
-    person_age = person[1]
+    person_age = int(person[1])
+
+    if person_age < youngest:
+        youngest = person_age
+        youngest_name = person_name
+
+print(f"\nThe youngest person is '{youngest_name}' with {youngest} years old.")
+
+
 
 
---
+++ w06/test2.py
@@ -0,0 +1,5 @@
+line = "     text"
+
+text = line.strip()
+
+print(line)
